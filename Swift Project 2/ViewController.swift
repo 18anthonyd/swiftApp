df4c2639c0ae39bd0df38b158b565159b3d0eadf
Dominic <18anthonyd@badinhs.org>
2016-09-14 08:20:47 -0400

    This is the first project thing

--- Swift Project 2/ViewController.swift
+++ Swift Project 2/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func button1Tapped(_ sender: AnyObject) {
+        coolLabel.text = "Now Cooler!"
+        print("Button Tapped!!")
+       tapCount = tapCount + 1
+        if tapCount == 10 {
+            coolLabel.text = "You've tapped ten times."
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.darkGray
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
